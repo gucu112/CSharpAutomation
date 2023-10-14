ab2d98c8c16724740d5a791410aad06af09e6231
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-14 21:13:02 +0200

    Add project files.

---
+++ PlaywrightXunit/TestResult.cs
@@ -0,0 +1,27 @@
+namespace Gucu112.PlaywrightXunit;
+
+public class TestResult
+{
+    [Fact]
+    public void TestPasses()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void TestFails()
+    {
+        Assert.True(false);
+    }
+
+    [Fact(Skip = "Test is skipped")]
+    public void TestIsSkipped()
+    {
+    }
+
+    [SkippableFact]
+    public void TestIsSkippable()
+    {
+        Skip.If(true, "Test is skipped");
+    }
+}
