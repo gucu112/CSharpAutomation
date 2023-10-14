38f1bdd21467de8e3be6078845ebdb26eb233c4d
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-14 22:00:43 +0200

    Use ITestOutputHelper interface

--- PlaywrightXunit/TestResult.cs
+++ PlaywrightXunit/TestResult.cs
@@ -2,15 +2,24 @@ namespace Gucu112.PlaywrightXunit;
 
 public class TestResult
 {
+    private readonly ITestOutputHelper output;
+
+    public TestResult(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
     [Fact]
     public void TestPasses()
     {
         Assert.True(true);
+        output.WriteLine("Test have passed");
     }
 
     [Fact]
     public void TestFails()
     {
+        output.WriteLine("Test is about to fail");
         Assert.True(false);
     }
 
