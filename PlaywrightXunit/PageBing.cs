b268cf6a846002d84fe0936dc73b5d822a64acb1
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-15 09:02:45 +0200

    Implement BingPage

---
+++ PlaywrightXunit/PageBing.cs
@@ -0,0 +1,25 @@
+﻿using Gucu112.PlaywrightXunit.Fixtures;
+using Gucu112.PlaywrightXunit.Pages;
+
+namespace Gucu112.PlaywrightXunit;
+
+public class PageBing : PageBase, IAsyncLifetime
+{
+    public string BaseUrl { get; private set; }
+
+    public PageBing(PlaywrightFixture fixture) : base(fixture)
+    {
+        BaseUrl = Settings.GetTestParameter("BingBaseURL");
+    }
+
+    public new async Task InitializeAsync()
+    {
+        await base.InitializeAsync();
+        await Context.GotoAsync(BaseUrl);
+        await AcceptCookiesButton.ClickAsync();
+    }
+
+    public ILocator AcceptCookiesButton => Context.Locator("button[id=bnp_btn_accept]");
+    public ILocator SearchInput => Context.Locator("textarea[id=sb_form_q]");
+    public ILocator SearchResult => Context.Locator("li[data-tag] h2>a[href]");
+}
---
+++ PlaywrightXunit/TestBing.cs
@@ -0,0 +1,39 @@
+﻿using Gucu112.PlaywrightXunit.Fixtures;
+
+namespace Gucu112.PlaywrightXunit.Tests;
+
+[Collection(nameof(PlaywrightFixture))]
+public class TestBing : IClassFixture<PageBing>
+{
+    private readonly PageBing page;
+
+    public TestBing(PageBing page)
+    {
+        this.page = page;
+    }
+
+    [Fact]
+    public async Task VerifyThatSearchInputIsVisible()
+    {
+        // Act
+        var isVisible = await page.SearchInput.IsVisibleAsync();
+
+        // Assert
+        Assert.True(isVisible);
+    }
+
+    [Fact]
+    public async Task VerifyThatSearchResultsContainSearchPhrase()
+    {
+        // Arrange
+        var phrase = "cat";
+        await page.SearchInput.FillAsync(phrase);
+        await page.Context.Keyboard.PressAsync("Enter");
+
+        // Act
+        var results = await page.SearchResult.AllTextContentsAsync() as IEnumerable<string>;
+
+        // Assert
+        Assert.All(results, result => Assert.Contains(phrase, result, StringComparison.InvariantCultureIgnoreCase));
+    }
+}
