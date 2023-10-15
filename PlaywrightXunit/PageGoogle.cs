c6465f4eea228ae73fb13daaeacc6b1ca22c9147
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-15 07:42:40 +0200

    Implement GooglePage

---
+++ PlaywrightXunit/PageGoogle.cs
@@ -0,0 +1,33 @@
+﻿using Gucu112.PlaywrightXunit.Fixtures;
+using Gucu112.PlaywrightXunit.Pages;
+
+namespace Gucu112.PlaywrightXunit;
+
+public class PageGoogle : PageBase, IAsyncLifetime
+{
+    public string BaseUrl { get; private set; }
+
+    public PageGoogle(PlaywrightFixture fixture) : base(fixture)
+    {
+        BaseUrl = Settings.GetTestParameter("GoogleBaseURL");
+    }
+
+    public new async Task InitializeAsync()
+    {
+        await base.InitializeAsync();
+        await BrowserContext.AddCookiesAsync(new List<Cookie>
+        {
+            new Cookie()
+            {
+                Name = "SOCS",
+                Value = "CAISHA",
+                Domain = ".google.com",
+                Path = "/"
+            }
+        });
+        await Context.GotoAsync(BaseUrl);
+    }
+
+    public ILocator SearchInput => Context.Locator("textarea[name=q]");
+    public ILocator SearchResult => Context.Locator("h3[class^=LC20lb]");
+}
---
+++ PlaywrightXunit/TestGoogle.cs
@@ -0,0 +1,41 @@
+﻿using Gucu112.PlaywrightXunit.Fixtures;
+
+namespace Gucu112.PlaywrightXunit.Tests;
+
+[Collection(nameof(PlaywrightFixture))]
+public class TestGoogle : IClassFixture<PageGoogle>
+{
+    private readonly PageGoogle page;
+
+    public TestGoogle(PageGoogle page)
+    {
+        this.page = page;
+    }
+
+    [Fact]
+    public async Task VerifyIfSearchInputIsVisible()
+    {
+        // Act
+        var isVisible = await page.SearchInput.IsVisibleAsync();
+
+        // Assert
+        Assert.True(isVisible);
+    }
+
+    [Theory]
+    [InlineData("c")]
+    [InlineData("d")]
+    [InlineData("e")]
+    public async Task VerifyIfSearchResultContainsSearchPhrase(string phrase)
+    {
+        // Arrange
+        await page.SearchInput.FillAsync(phrase);
+        await page.Context.Keyboard.PressAsync("Enter");
+
+        // Act
+        var results = await page.SearchResult.AllTextContentsAsync() as IEnumerable<string>;
+
+        // Assert
+        Assert.All(results, result => Assert.Contains(phrase, result, StringComparison.InvariantCultureIgnoreCase));
+    }
+}
