7daff8baa5627617502978d74652305c0f69eb58
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-15 03:16:58 +0200

    Implement Playwright fixture

---
+++ PlaywrightXunit/PlaywrightFixture.cs
@@ -0,0 +1,31 @@
+﻿namespace Gucu112.PlaywrightXunit.Fixtures;
+
+public class PlaywrightFixture : IAsyncLifetime
+{
+    private readonly Settings settings;
+
+    public PlaywrightFixture()
+    {
+        settings = new Settings();
+    }
+
+    private IPlaywright Instance { get; set; } = null!;
+    public IBrowser Browser { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        Instance = await Playwright.CreateAsync();
+        Browser = await Instance["firefox"].LaunchAsync(settings.GetLaunchOptions());
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Browser.DisposeAsync();
+        Instance.Dispose();
+    }
+}
+
+[CollectionDefinition(nameof(PlaywrightFixture))]
+public class PlaywrightCollection : ICollectionFixture<PlaywrightFixture>
+{
+}
---
+++ PlaywrightXunit/TestPlaywright.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Gucu112.PlaywrightXunit.Fixtures;
+
+namespace Gucu112.PlaywrightXunit.Tests;
+
+[Collection(nameof(PlaywrightFixture))]
+public class TestPlaywright
+{
+    private readonly PlaywrightFixture fixture;
+
+    public TestPlaywright(PlaywrightFixture fixture)
+    {
+        this.fixture = fixture;
+    }
+
+    [Fact]
+    public void VerifyIfPlaywrightInstanceInitilized()
+    {
+        // Arrange
+        var property = fixture.GetType().GetProperty("Instance",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+
+        // Act
+        var instance = property?.GetGetMethod(nonPublic: true)?.Invoke(fixture, null);
+
+        // Assert
+        Assert.IsAssignableFrom<IPlaywright>(instance);
+    }
+
+    [Fact]
+    public void VerifyIfBrowserInitilizedAndConnected()
+    {
+        // Arrange
+        var browser = fixture.Browser;
+
+        // Assert
+        Assert.IsAssignableFrom<IBrowser>(browser);
+        Assert.True(browser.IsConnected);
+    }
+}
