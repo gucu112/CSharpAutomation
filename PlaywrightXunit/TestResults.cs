ad26b52f637f7d4b3522a6626544cc42086c83b0
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-15 04:59:47 +0200

    Add timeout fact in TestResults

--- PlaywrightXunit/TestResults.cs
+++ PlaywrightXunit/TestResults.cs
@@ -10,27 +10,34 @@ public class TestResults
     }
 
     [Fact]
-    public void TestPasses()
+    public void TestPass()
     {
         Assert.True(true);
-        output.WriteLine("Test have passed");
+        output.WriteLine("Test has passed");
     }
 
     [Fact]
-    public void TestFails()
+    public void TestFail()
     {
         output.WriteLine("Test is about to fail");
         Assert.Fail();
     }
 
     [Fact(Skip = "Test is skipped using Fact")]
-    public void TestIsSkipped()
+    public void TestSkip()
     {
     }
 
     [SkippableFact]
-    public void TestIsSkippable()
+    public void TestSkippable()
     {
         Skip.If(true, "Test is skipped using SkippableFact");
+    }
+
+    [Fact(Timeout = 1000)]
+    public async Task TestTimeout()
+    {
+        output.WriteLine("Test is about to wait 3000 milliseconds");
+        await Task.Delay(3000);
     }
 }
