a0e1514da19cde5c054cc19bed954ba54d1e878f
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-15 05:24:59 +0200

    Implement BasePage

---
+++ PlaywrightXunit/PageBase.cs
@@ -0,0 +1,30 @@
+﻿using Gucu112.PlaywrightXunit.Fixtures;
+
+namespace Gucu112.PlaywrightXunit.Pages;
+
+public abstract class PageBase : IAsyncLifetime
+{
+    private readonly Settings settings;
+
+    protected IBrowser Browser { get; set; } = null!;
+    public IBrowserContext BrowserContext { get; private set; } = null!;
+    public IPage Context { get; private set; } = null!;
+
+    public PageBase(PlaywrightFixture fixture)
+    {
+        settings = new Settings();
+        Browser = fixture.Browser;
+    }
+
+    public async Task InitializeAsync()
+    {
+        BrowserContext = await Browser.NewContextAsync();
+        BrowserContext.SetDefaultTimeout(settings.GetExpectTimeout());
+        Context = await BrowserContext.NewPageAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await BrowserContext.DisposeAsync();
+    }
+}
--- PlaywrightXunit/TestPlaywright.cs
+++ PlaywrightXunit/TestPlaywright.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using Gucu112.PlaywrightXunit.Fixtures;
+using Gucu112.PlaywrightXunit.Pages;
 
 namespace Gucu112.PlaywrightXunit.Tests;
 
@@ -30,7 +31,7 @@ public class TestPlaywright
     [Fact]
     public void VerifyIfBrowserInitilizedAndConnected()
     {
-        // Arrange
+        // Act
         var browser = fixture.Browser;
 
         // Assert
@@ -42,10 +43,68 @@ public class TestPlaywright
     public void VerifyIfCorrectBrowserTypeOpened()
     {
         // Arrange
+        var settings = new Settings();
+
+        // Act
         var browser = fixture.Browser;
 
         // Assert
-        Assert.IsAssignableFrom<IBrowser>(browser);
-        Assert.Equal("firefox", browser.BrowserType.Name);
+        Assert.Equal(settings.GetBrowserName(), browser.BrowserType.Name);
+    }
+
+    [Fact]
+    public async Task VerifyIfBrowserContextInitialized()
+    {
+        // Arrange
+        var page = new PageBlank(fixture);
+        await page.InitializeAsync();
+
+        // Act
+        var browserContext = page.BrowserContext;
+        await page.DisposeAsync();
+
+        // Assert
+        Assert.IsAssignableFrom<IBrowserContext>(browserContext);
+    }
+
+    [Fact]
+    public async Task VerifyIfPageContextInitialized()
+    {
+        // Arrange
+        var page = new PageBlank(fixture);
+        await page.InitializeAsync();
+
+        // Act
+        var pageContext = page.Context;
+        await page.DisposeAsync();
+
+        // Assert
+        Assert.IsAssignableFrom<IPage>(pageContext);
+    }
+
+    [Fact]
+    public async Task VerifyIfPageTimeoutIsCorrect()
+    {
+        // Arrange
+        var settings = new Settings();
+        var page = new PageBlank(fixture);
+        await page.InitializeAsync();
+
+        // Act
+        await page.Context.GotoAsync(settings.GetTestParameter("BaseURL"));
+        var action = async () =>
+        {
+            await page.Context.GetByTestId("unknow").ClickAsync();
+            await page.DisposeAsync();
+        };
+
+        // Assert
+        Exception exception = await Assert.ThrowsAsync<TimeoutException>(action);
+        Assert.Contains($"Timeout {settings.GetExpectTimeout()}ms exceeded", exception.Message);
+    }
+
+    private class PageBlank : PageBase
+    {
+        public PageBlank(PlaywrightFixture fixture) : base(fixture) { }
     }
 }
