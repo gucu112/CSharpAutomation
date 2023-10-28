226a077c103eb9835bbc20b6bcf02098982e6421
Bartlomiej Roszczypala <gucu112@hotmail.com>
2023-10-28 18:49:09 +0200

    Implement BasePage

---
+++ PlaywrightXunitParallel/Pages/BasePage.cs
@@ -0,0 +1,35 @@
+﻿using Gucu112.PlaywrightXunitParallel.Fixtures;
+
+namespace Gucu112.PlaywrightXunitParallel.Pages;
+
+public abstract class BasePage : IAsyncLifetime
+{
+    protected SettingsFixture Settings { get; set; }
+    protected IBrowser Browser { get; set; } = null!;
+    public IBrowserContext BrowserContext { get; private set; } = null!;
+    public IPage Context { get; private set; } = null!;
+    public BrowserNewContextOptions BrowserOptions { get; private set; } = null!;
+
+    public BasePage(
+        SettingsFixture settingsFixture,
+        PlaywrightFixture playwrightFixture
+    )
+    {
+        Settings = settingsFixture;
+        BrowserOptions = settingsFixture.BrowserOptions;
+        Browser = playwrightFixture.Browser;
+    }
+
+    public async Task InitializeAsync()
+    {
+        BrowserContext = await Browser.NewContextAsync(BrowserOptions);
+        BrowserContext.SetDefaultTimeout(Settings.ExpectTimeout);
+        Context = await BrowserContext.NewPageAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Context.CloseAsync();
+        await BrowserContext.DisposeAsync();
+    }
+}
--- PlaywrightXunitParallel/Tests/Core/PlaywrightTest.cs
+++ PlaywrightXunitParallel/Tests/Core/PlaywrightTest.cs
@@ -1,20 +1,26 @@
 ﻿using Gucu112.PlaywrightXunitParallel.Fixtures;
+using Gucu112.PlaywrightXunitParallel.Pages;
 
 namespace Gucu112.PlaywrightXunitParallel.Tests.Core;
 
 [Collection(nameof(SettingsFixture))]
 public class PlaywrightTest : IClassFixture<PlaywrightFixture>
 {
+    private readonly SettingsFixture settings;
     private readonly PlaywrightFixture playwright;
 
-    public PlaywrightTest(PlaywrightFixture fixture)
+    public PlaywrightTest(
+        SettingsFixture settingsFixture,
+        PlaywrightFixture playwrightFixture)
     {
-        playwright = fixture;
+        settings = settingsFixture;
+        playwright = playwrightFixture;
     }
 
     [Fact]
-    public void VerifyThatBrowserIsConnected()
+    public void VerifyThatBrowserDoesExist()
     {
+        Assert.NotNull(playwright.Browser);
         Assert.True(playwright.Browser.IsConnected, "Browser is not connected");
         Assert.Equal("chromium", playwright.Browser.BrowserType.Name);
     }
@@ -24,5 +30,60 @@ public class PlaywrightTest : IClassFixture<PlaywrightFixture>
     {
         Assert.NotNull(playwright.LaunchOptions);
         Assert.Equal("msedge", playwright.LaunchOptions.Channel);
+    }
+
+    [Fact]
+    public void VerifyThatBrowserContextDoesExist()
+    {
+        using var page = new BlankPage(settings, playwright);
+
+        Assert.NotNull(page.BrowserContext);
+        Assert.Equal("chromium", page.BrowserContext.Browser?.BrowserType.Name);
+    }
+
+    [Fact]
+    public void VerifyThatBrowserOptionsAreInitialized()
+    {
+        using var page = new BlankPage(settings, playwright);
+
+        Assert.NotNull(page.BrowserOptions);
+        Assert.False(page.BrowserOptions.Offline, "Browser is in offline mode");
+    }
+
+    [Fact]
+    public void VerifyThatPageDoesExist()
+    {
+        using var page = new BlankPage(settings, playwright);
+
+        Assert.NotNull(page.Context);
+        Assert.Equal("about:blank", page.Context.Url);
+    }
+
+    [Fact]
+    public async Task VerifyThatPageTimeoutWorksCorrect()
+    {
+        using var page = new BlankPage(settings, playwright);
+        var action = async () => await page.Context.GetByTestId("unknow").ClickAsync();
+
+        await page.Context.GotoAsync("chrome://about");
+
+        Exception exception = await Assert.ThrowsAsync<TimeoutException>(action);
+        Assert.Contains($"Timeout {settings.ExpectTimeout}ms exceeded", exception.Message);
+    }
+
+    private class BlankPage : BasePage, IDisposable
+    {
+        public BlankPage(
+            SettingsFixture settings,
+            PlaywrightFixture playwright
+        ) : base(settings, playwright)
+        {
+            Task.Run(async () => await InitializeAsync()).Wait();
+        }
+
+        public void Dispose()
+        {
+            Task.Run(async () => await DisposeAsync()).Wait();
+        }
     }
 }
