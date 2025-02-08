2569603b7d0aaebf6380e95e36740c59f49c6cc1
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-02-08 19:07:24 +0100

    Implement Parse.FromXml function using string content input

---
+++ Common/Helpers.Tests/Extensions/BoolExtensionsTest.cs
@@ -0,0 +1,22 @@
+using Gucu112.CSharp.Automation.Helpers.Extensions;
+
+namespace Gucu112.CSharp.Automation.Helpers.Tests.Extensions;
+
+[TestFixture]
+public class BoolExtensionsTest
+{
+    [TestCase(true, ExpectedResult = "Yes")]
+    [TestCase(false, ExpectedResult = "No")]
+    public string ToLocalizedString_DoesReturnRegular(bool value)
+    {
+        return BoolExtensions.ToLocalizedString(value);
+    }
+
+    [TestCase(null, ExpectedResult = null)]
+    [TestCase(true, ExpectedResult = "Yes")]
+    [TestCase(false, ExpectedResult = "No")]
+    public string? ToLocalizedString_DoesReturnNullable(bool? value)
+    {
+        return BoolExtensions.ToLocalizedString(value);
+    }
+}
--- Common/Helpers.Tests/Usings.cs
+++ Common/Helpers.Tests/Usings.cs
@@ -1,6 +1,9 @@
 global using System.Collections;
 global using System.Text;
+global using System.Xml;
+global using System.Xml.Linq;
 global using Moq;
 global using Newtonsoft.Json;
 global using Newtonsoft.Json.Linq;
+global using JsonFormatting = Newtonsoft.Json.Formatting;
 global using StringWriter = Gucu112.CSharp.Automation.Helpers.Models.StringWriter;
